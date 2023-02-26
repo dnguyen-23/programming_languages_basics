4d9ac69c57cebb7efb4940a1e492682e9d90db8d
Daniel Nguyen <dnguyen23@hanoverstudents.org>
2023-02-26 18:38:31 -0500

    safe

--- lang_w_functions/_build/default/bin/main.ml
+++ lang_w_functions/_build/default/bin/main.ml
@@ -4,6 +4,8 @@ let pretty_print inp_str =
   Caml.print_endline "";
   Caml.print_endline inp_str;
 
+let all_variables = [];;
+
 type term = 
 | Tnum of int
 | Tbool of bool
@@ -12,10 +14,14 @@ type term =
 | For_loop of term * term 
 (* Binary Operators *)
 | Add of term * term
-| Equals of term * term
-(* | ID of string * term 1.) name of the identifier; 2.) a body of terms or simply a term *)
+| Equality of term * term
+| Assign of term * term
+(* 1.) name of the identifier; 2.) a body of terms or simply a term *)
+(* Should create a List for all identifiers created *)
+| ID of string * term 
 (* Body expression *)
 | Body of term list
+
 
 
 
@@ -37,14 +43,32 @@ let rec parse (inp_sexp : Sexp.t) : term =
       For_loop (p_num_iter, p_body_expr)
     (* 2.) parse binary operators *)
     | [t1; operator; t2] ->
-      let p_t1 = parse t1 in
-      let p_t2 = parse t2 in
       (match operator with
-      | Sexp.Atom "+" -> Add(p_t1, p_t2)
-      | Sexp.Atom "==" -> Equals(p_t1, p_t2)
+      | Sexp.Atom "+" -> 
+        let p_t1 = parse t1 in
+        let p_t2 = parse t2 in
+        Add(p_t1, p_t2)
+      | Sexp.Atom "==" -> 
+        let p_t1 = parse t1 in
+        let p_t2 = parse t2 in
+        Equality(p_t1, p_t2)
+      | Sexp.Atom "=" -> 
+        let p_t2 = parse t2 in
+        (match t2 with
+        | Sexp.Atom a ->
+          Assign(a, p_t2)
+        | _ -> failwith "Error: Invalid assignment"
+        )
       | _ -> failwith "not a binary operator"
       )
-    (* 3.) body of expressions *)
+    (* 3.) variable *)
+    | [Sexp.Atom "let"; Sexp.Atom name; Sexp.Atom "="; value] ->
+      let p_val = parse value in
+      (* this parse function must also return a list of variables created *)
+      let res_var = ID(name, p_val)
+
+    
+    (* 4.) body of expressions *)
     | _ -> Body (List.map ~f:parse l) (* parse each element in the list*)
     
     
@@ -81,7 +105,7 @@ let rec string_parse_result buffer t =
     let final_buffer = string_parse_result buffer_w_op t2 in
     let final = final_buffer ^ ")" in
     final
-  | Equals(t1, t2)-> 
+  | Equality(t1, t2)-> 
     let temp_buffer = buffer ^ "(" in
     let buffer_w_t1 = string_parse_result temp_buffer t1 in
     let buffer_w_op = buffer_w_t1 ^ " == " in
@@ -102,6 +126,52 @@ let rec string_parse_result buffer t =
     ;;
 ;;
 
+(* t is a series of term types *)
+let rec eval t =
+  match t with
+  | Tnum _ -> t
+  | Tbool _ -> t
+  | Add (t1, t2) ->
+    let e_t1 = eval t1 in
+    let e_t2 = eval t2 in
+    (match (e_t1, e_t2) with
+    | (Tnum n1, Tnum n2) -> Tnum(n1 + n2)
+    | _ -> failwith "Error: Adding"
+    )
+  | Equality (t1, t2) -> 
+    let e_t1 = eval t1 in
+    let e_t2 = eval t2 in
+    (match (e_t1, e_t2) with
+    | (Tnum n1, Tnum n2) -> 
+      if n1 = n2 then Tbool(true) else Tbool(false)
+    | (Tbool b1, Tbool b2) ->
+      if b1 = b2 then Tbool(true) else Tbool(false)
+    | _ -> failwith "Error: Equality"
+    
+    )
+  | Body l -> Body(List.iter ~f:parse l) (* This is only a temporary solution *)
+                                          (* Body should return a single value *)
+  | If_else (condition, body_expr1, body_expr2) -> 
+    let e_condition = eval condition in
+    (match e_condition with
+    | Tbool b -> 
+      if b then (eval body_expr1) else (eval body_expr2)
+    | _ -> failwith "Error: Condition is not a boolean"
+    )
+    (* | For_loop (num_iterations, body_expr) ->
+    let e_iterations = eval num_iterations in
+    (match e_iterations with
+    | Tnum n -> 
+      let rec for_loop_func i =
+        if i = -1 then
+
+        else 
+          eval  
+    | _ -> failwith "Error: iterations expects a number"
+
+    )  *)
+  | 
+
 (* Caml.print_endline "";
 let bufFor = For_loop(Tnum(5), Body([Add(Tnum(5), Tnum(1))])) in
 let r = string_parse_result "" bufFor in
@@ -116,7 +186,7 @@ Caml.print_endline result_1 ;;
 Caml.print_endline "\n\n" ;;
 
 
-let expression = If_else (Equals(Tnum (1), Tnum (2)), Add(Tnum(12), Tnum(23)), Add(Tnum(100), Tnum(200))) in
+let expression = If_else (Equality(Tnum (1), Tnum (2)), Add(Tnum(12), Tnum(23)), Add(Tnum(100), Tnum(200))) in
 let buffer = "" in
 let result = string_parse_result buffer expression in
 Caml.print_endline result ;; *)
